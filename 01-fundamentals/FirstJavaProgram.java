42c51656cdf033c1ae558bd7670b1a4a4c9f47fc
Brian Cushman <cushman.brian@gmail.com>
2017-07-05 13:02:18 -0700

    Added FirstJavaProgram

---
+++ 01-fundamentals/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+// Print your name, age, and hometown
+
+public class FirstJavaProgram {
+	public static void main(String[] args) {
+		System.out.println("My name is Brian Cushman.");
+		System.out.println("I am many years old.");
+		System.out.println("My hometown is Burbank, CA.");
+	}
+}
